cb8fed4f54f26481e88f5f4e5eae901f58e5cff2
Gustavo Casarin <gustavocasarinsilva@gmail.com.br>
2026-05-06 13:05:07 -0300

    teste comentario

--- src/controller/login-controller.ts
+++ src/controller/login-controller.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from "express";
 import { LoginService } from "../service/login-service";
 
+//teste
+
 export class LoginController {
     private service: LoginService;
 
